4fb63294a7ae14a8c76c5f2e15beb097a5237f0e
charlotterichy <charlotte.richmond2000@gmail.com>
2020-03-04 18:36:43 +0000

    final2

--- charellie.js
+++ charellie.js
@@ -1,19 +1,4 @@
 
-
-
-function changeColor(obj) {
-
-if (obj.style.color == 'black') {
-    obj.style.color = 'white';
-
-    } else {
-
-    obj.style.color = 'black';
-
-    }
-
-}
-
 // forms.js
 //
 
